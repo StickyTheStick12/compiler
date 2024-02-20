bc6d55328caa42e88f029625140436a085da8e4f
kevin wigren <89857252+StickyTheStick12@users.noreply.github.com>
2024-02-20 08:56:18 +0100

    Update semantic.cpp

--- Assignment 2/task 2/semantic.cpp
+++ Assignment 2/task 2/semantic.cpp
@@ -1,5 +1,7 @@
 #include "semantic.h"
 
+//kolla assignment så rhs inte är void heller
+
 std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
 {
     if(node->type == "Program")
@@ -12,7 +14,7 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         }
         return "void";
     }
-    else if(node->type == "Class" || node->type == "Class body" || node->type == "Method statement" || node->type == "Statements" || node->type == "Main class statement" || node->type == "MethodList" || node->type == "Class variables")
+    else if(node->type == "Class" || node->type == "Class body" || node->type == "Method statement" || node->type == "Statements" || node->type == "Main class statement" || node->type == "MethodList" || node->type == "Class variables" || node->type == "Print")
     {
         for(auto & iter : node->children)
             TraverseTreeSemantic(ST, iter);
@@ -46,11 +48,8 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto returnType = TraverseTreeSemantic(ST, node->children.back());
 
         if(node->children.front()->value != returnType)
-        {
-            std::cout << "TypeError: return value differs from expected type. Cannot convert" << returnType << " to " <<
-                      node->children.front()->value << std::endl;
-            exit(-1);
-        }
+            std::cout << "Return type of method doesnt match type of returned value. Cannot convert " << returnType <<
+            " to " << node->children.front()->value << std::endl;
 
         return node->children.front()->value;
     }
@@ -63,32 +62,26 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
     }
     else if(node->type == "If statement" || node->type == "If else" || node->type == "While loop")
     {
-        for(auto & iter : node->children)
-            TraverseTreeSemantic(ST, iter);
+        for(auto iter = std::next(node->children.begin()); iter != node->children.end(); ++iter)
+            TraverseTreeSemantic(ST, *iter);
 
         auto lhs = TraverseTreeSemantic(ST, node->children.front());
 
         if(lhs != "bool")
-        {
-            std::cout << "Cannot convert " << lhs << " to a bool" << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: if and while require a bool. Cannot convert " << lhs << " to a bool" << std::endl;
 
         return "void";
     }
     else if(node->type == "Assignment")
     {
-        //todo: check if this is a error
         auto lhs = TraverseTreeSemantic(ST, node->children.front());
-        auto rhs = TraverseTreeSemantic(ST, node->children.back());
 
+        if(lhs == "void")
+            return "void";
+
+        auto rhs = TraverseTreeSemantic(ST, node->children.back());
         if(lhs != rhs)
-        {
-            std::cout << "TypeError: " << lhs << " type does not match with " << rhs << " type" << std::endl;
-            std::cout << node->children.front()->type << std::endl;
-            std::cout << node->children.back()->type << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: " << lhs << " type does not match with " << rhs << " type" << std::endl;
 
         return "void";
     }
@@ -99,20 +92,11 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto rhs = TraverseTreeSemantic(ST, node->children.back());
 
         if(lhs != "int[]")
-        {
-            std::cout << "TypeError: the type of the expression must be an array but is a " << lhs << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: the type of the expression must be an int array but is a " << lhs << std::endl;
         else if(idx != "int")
-        {
-            std::cout << "TypeError: Cannot convert " << idx << " to a integer" <<  std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Cannot convert " << idx << " to a integer. Idx for the int array must be an int" <<  std::endl;
         else if(rhs != "int")
-        {
-            std::cout << "TypeError: Cannot convert " << rhs << " to a integer" <<  std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Cannot convert " << rhs << " to a integer" <<  std::endl;
 
         return "void";
     }
@@ -122,10 +106,8 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto rhs = TraverseTreeSemantic(ST, node->children.back());
 
         if(lhs != "bool" || rhs != "bool")
-        {
-            std::cout << "TypeError: unsupported operands: " << lhs << " and " << rhs << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: unsupported operands for 'or' or 'and' expression. Can't convert " << lhs << " or "
+            << rhs << " to a bool" << std::endl;
 
         return "bool";
     }
@@ -135,10 +117,7 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto rhs = TraverseTreeSemantic(ST, node->children.back());
 
         if(lhs != rhs)
-        {
-            std::cout << "TypeError: Unsupported operand types for ==: " << lhs << " and " << rhs << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: == requires that both types are the same." << lhs << " and " << rhs << " is not" << std::endl;
 
         return "bool";
     }
@@ -148,10 +127,7 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto rhs = TraverseTreeSemantic(ST, node->children.back());
 
         if(lhs != "int" || rhs != "int")
-        {
-            std::cout << "TypeError: Unsupported operand types for ==: " << lhs << " and " << rhs << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Cannot convert " << lhs << " or " << rhs << " to an int for LT or GT" << std::endl;
 
         return "bool";
     }
@@ -162,10 +138,8 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto rhs = TraverseTreeSemantic(ST, node->children.back());
 
         if(lhs != "int" || rhs != "int")
-        {
-            std::cout << "TypeError: Unsupported operand types for +, -, * or /: " << lhs << " and " << rhs << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Unsupported operand type for +, -, * or /. Cannot convert to an int: " << lhs <<
+            " or " << rhs << std::endl;
 
         return "int";
     }
@@ -174,10 +148,7 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto lhs = TraverseTreeSemantic(ST, node->children.front());
 
         if(lhs != "bool")
-        {
-            std::cout << "TypeError: Unsupported operand type for !: " << lhs << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Unsupported operand type for !: " << lhs << std::endl;
 
         return "bool";
     }
@@ -189,47 +160,57 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto idx = TraverseTreeSemantic(ST, node->children.back());
 
         if(lhs != "int[]")
-        {
-            std::cout << "Type must be an array but is a " << lhs << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Type must be an array but is a " << lhs << std::endl;
         else if(idx != "int")
-        {
-            std::cout << "Type must be an int but is a " << idx << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Type must be an int but is a " << idx << std::endl;
 
         return "int";
     }
     else if(node->type == "Method call") {
+        auto callerSymbol = TraverseTreeSemantic(ST, node->children.front());
+        auto callerClass = (Class*)ST->Lookup((ST->Lookup(callerSymbol))->GetType());
+
+
         auto caller = TraverseTreeSemantic(ST, node->children.front());
         auto callerRecord = ST->Lookup(caller);
-        auto callerClass = (Class*)ST->Lookup(callerRecord->GetType());
+
+        if(callerClass != (Class*)ST->Lookup(callerRecord->GetType()))
+            std::cout << "WRONG" << std::endl;
+
+        //auto callerClass = (Class*)ST->Lookup(callerRecord->GetType());
         if (callerClass == nullptr) {
             cout << "Error: Method " << (*(++node->children.begin()))->value << " is not defined on instance of type " << callerRecord->GetType() << endl;
-            exit(-1);
+            return "void";
         }
-        auto childrenFront = node->children.begin();
-        advance(childrenFront, 1);
-        auto method = callerClass->MethodLookup((*childrenFront)->value);
-        if (method == nullptr) {
-            cout << "Error: Method " << (*childrenFront)->value << " is not defined on instance of type " << callerRecord->GetType() << endl;
-            exit(-1);
+
+        auto childIter = std::next(node->children.begin());
+        auto method = callerClass->MethodLookup((*childIter)->value);
+
+        if(method == nullptr)
+        {
+            std::cout << "Error: method " << (*childIter)->value << " is not defined instance " << node->children.front()->value << std::endl;
+            return "void";
         }
-        advance(childrenFront, 1);
-        if (childrenFront == node->children.end()) {
-            if (method->GetNumberParameters() != 0) {
-                cout << "Not the correct number of parameters for method " << method->GetId() << endl;
+
+        childIter = std::next(childIter);
+
+        if(childIter == node->children.end())
+        {
+            if(method->GetNumberParameters() != 0)
+            {
+                std::cout << "Error: method expects 0 arguments, 1 or more were provided for method " << method->GetId()
+                << std::endl;
                 exit(-1);
+                return "void";
             }
-        } else if (method->GetNumberParameters() != (*childrenFront)->children.size()) {
+        }
+        else if (method->GetNumberParameters() != (*childIter)->children.size()) {
+            std::cout << method->GetNumberParameters() << std::endl;
             //std::cout << "Not the correct number of parameters for method " << method->GetId() << std::endl;
-            //std::cout << (*childrenFront)->children.size() << std::endl;
-            //std::cout << method->GetNumberParameters() << std::endl;
             //exit(-1);
         } else {
             int idx = 0;
-            for (auto i=(*childrenFront)->children.begin(); i!=(*childrenFront)->children.end(); i++) {
+            for (auto i=(*childIter)->children.begin(); i!=(*childIter)->children.end(); i++) {
                 auto type = TraverseTreeSemantic(ST, *i);
                 if (type != method->GetParameter(idx)->GetType()) {
                     cout << "TypeError: type missmatch, '" << type << "' does not match with '" << method->GetParameter(idx)->GetType() << "' in method call " << method->GetId() << endl;
@@ -245,10 +226,7 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto lhs = TraverseTreeSemantic(ST, node->children.front());
 
         if(lhs != "int[]")
-        {
-            std::cout << "This type does not have a field length" << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: " << lhs << " does not have a field length." << std::endl;
 
         return "int";
     }
@@ -257,10 +235,7 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         auto lhs = TraverseTreeSemantic(ST, node->children.front());
 
         if(lhs != "int")
-        {
-            std::cout << "Cannot convert " << lhs << " to int" << std::endl;
-            exit(-1);
-        }
+            std::cout << "Error: Cannot convert " << lhs << " to an int" << std::endl;
 
         return "int[]";
     }
@@ -268,15 +243,15 @@ std::string TraverseTreeSemantic(SymbolTable* ST, Node* node)
         return (*node->children.begin())->value;
     else if(node->type == "ID" || node->type == "THIS")
     {
-        auto id = ST->Lookup(node->value);
+        auto idPtr = ST->Lookup(node->value);
 
-        if(id == nullptr)
+        if(idPtr == nullptr)
         {
             std::cout << "Error: identifier " << node->value << " is not defined" << std::endl;
-            exit(-1);
+            return "void";
         }
 
-        return id->GetType();
+        return idPtr->GetType();
     }
     else if(node->type == "Int")
         return "int";
